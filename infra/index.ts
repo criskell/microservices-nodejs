b53b200885e588e3a1ec80d33bc06835135066f0
criskell <cris.moraes.ds@gmail.com>
2025-06-08 07:21:52 -0300

    add invoices service

--- infra/index.ts
+++ infra/index.ts
@@ -1,11 +1,11 @@
-import * as pulumi from '@pulumi/pulumi';
-
 import { ordersService } from './src/services/orders';
+import { invoicesService } from './src/services/invoices';
 import { appLoadBalancer } from './src/load-balancer';
 import { rabbitMQService } from './src/services/rabbitmq';
 import { kongService } from './src/services/kong';
 
 export const ordersServiceId = ordersService.service.id;
+export const invoicesServiceId = invoicesService.service.id;
 export const rabbitMQId = rabbitMQService.service.id;
 export const kongId = kongService.service.id;
-export const rabbitMQAdminUrl = pulumi.interpolate`http://${appLoadBalancer.listeners[0].endpoint.hostname}:15672`;
+export const loadBalancerId = appLoadBalancer.loadBalancer.id;
---
+++ infra/src/services/invoices.ts
@@ -0,0 +1,91 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as awsx from '@pulumi/awsx';
+
+import { cluster } from '../cluster';
+import { invoicesDockerImage } from '../images/invoices';
+import { amqpListener } from './rabbitmq';
+import { appLoadBalancer } from '../load-balancer';
+
+const invoicesTargetGroup = appLoadBalancer.createTargetGroup(
+  'invoices-target',
+  {
+    port: 3334,
+    protocol: 'HTTP',
+    healthCheck: {
+      path: '/health',
+      protocol: 'HTTP',
+    },
+  }
+);
+
+export const invoicesHttpListener = appLoadBalancer.createListener(
+  'invoices-listener',
+  {
+    port: 3334,
+    protocol: 'HTTP',
+    targetGroup: invoicesTargetGroup,
+  }
+);
+
+export const invoicesService = new awsx.classic.ecs.FargateService(
+  'fargate-invoices',
+  {
+    cluster,
+    desiredCount: 1,
+    waitForSteadyState: false,
+    taskDefinitionArgs: {
+      container: {
+        image: invoicesDockerImage.ref,
+        // 1vCPU = 1024 milicores
+        cpu: 256,
+        memory: 512,
+        portMappings: [invoicesHttpListener],
+        environment: [
+          {
+            name: 'BROKER_URL',
+            value: pulumi.interpolate`amqp://${pulumi.secret(
+              'AMQP_USERNAME'
+            )}:${pulumi.secret('AMQP_PASSWORD')}@${
+              amqpListener.endpoint.hostname
+            }:${amqpListener.endpoint.port}`,
+          },
+          {
+            name: 'DATABASE_URL',
+            value: pulumi.secret('INVOICES_DATABASE_URL'),
+          },
+          {
+            name: 'OTEL_TRACES_EXPORTER',
+            value: 'otlp',
+          },
+          {
+            name: 'OTEL_EXPORTER_OTLP_ENDPOINT',
+            value: 'https://otlp-gateway-prod-sa-east-1.grafana.net/otlp',
+          },
+          {
+            name: 'OTEL_EXPORTER_OTLP_HEADERS',
+            value: pulumi.interpolate`Authorization=Basic ${pulumi.secret(
+              'GRAFANA_OTEL_API_KEY'
+            )}`,
+          },
+          {
+            name: 'OTEL_RESOURCE_ATTRIBUTES',
+            value:
+              'service.name=invoices,service.namespace=eventonodejs,deployment.environment=production',
+          },
+          {
+            name: 'OTEL_NODE_RESOURCE_DETECTORS',
+            value: 'env,host,os',
+          },
+          {
+            name: 'OTEL_SERVICE_NAME',
+            value: 'invoices',
+          },
+          {
+            name: 'OTEL_NODE_ENABLED_INSTRUMENTATIONS',
+            value: 'http,fastify,pg,amqplib',
+          },
+        ],
+      },
+    },
+  }
+);
